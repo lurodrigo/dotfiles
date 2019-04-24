afec64c0c6eac5127038afe585d37116df744851
Luiz Rodrigo de Souza <me@lurodrigo.com>
2019-04-23 22:14:31 -0300

    Update manjaro-post-install.sh

--- manjaro-post-install.sh
+++ manjaro-post-install.sh
@@ -3,11 +3,11 @@ sudo pacman-mirrors --fasttrack && sudo pacman -Syyu
 
 # instala alguns pacotes importantes
 sudo pacman -S evince atom base-devel nemo nemo-fileroller nemo-preview r \
-    rhythmbox shotwell telegram-desktop thunderbird tk vlc yaourt zsh \ 
-    texlive-most texlive-langextra texmaker virtualbox gedit \ 
-    jdk-openjdk jdk8-openjdk emacs scala clojure maven gradle caprine ntp \ 
-    gcc-fortran transmission-gtk rodi conky ttf-font-awesome \ 
-    arandr lightdm-gtk-greeter-settings perl-json-xs xclip \ 
+    rhythmbox shotwell telegram-desktop thunderbird tk vlc zsh \
+    texlive-most texlive-langextra texmaker gedit \
+    jdk-openjdk jdk8-openjdk emacs scala clojure maven gradle ntp \
+    gcc-fortran transmission-gtk conky kitty ttf-font-awesome \
+    arandr lightdm-gtk-greeter-settings perl-json-xs xclip \
     gtk-engine-murrine gtk-engines ed gnome-disk-utility --noconfirm
 
 sudo pacman -R hexchat eviewpdf palemoon-bin --noconfirm
@@ -27,11 +27,12 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/p
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python get-pip.py --user
 pip install --user i3ipc
+echo "export PATH=~/.local/bin:\$PATH" >> ~/.zshrc
 
 # yaourt: ferramenta para instalar pacotes da AUR
 ## chave do dropbox
 gpg --recv-keys 1C61A2656FB57B7E4DE0F4C1FC918B335044912E
-yaourt -S google-chrome rstudio-desktop-bin srm dropbox nemo-dropbox overgrive \
+yay -S google-chrome rstudio-desktop-bin srm dropbox nemo-dropbox overgrive \
     skypeforlinux-stable-bin flacon perl-anyevent-i3 whatsapp-nativefier \
     nerd-fonts-complete nano-syntax-highlighting-git nerd-fonts-complete \
     leiningen font-manager --noconfirm
@@ -43,5 +44,5 @@ cd /usr/lib
 sudo rm libORBit-2.so.0
 sudo ln -s libORBit-2.so.0.1.0 libORBit-2.so.0
 cd $current_dir
-yaourt -S gnome-perl perl-gnome2-wnck perl-goo-canvas perl-gtk2-imageview \
+yay -S gnome-perl perl-gnome2-wnck perl-goo-canvas perl-gtk2-imageview \
     perl-gtk2-unique shutter --noconfirm
